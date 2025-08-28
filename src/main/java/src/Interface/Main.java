f70ced520c771cd9c3846782b1d805328a36323c
Enzo-Mandelli <enzo.contato.me@gmail.com>
2025-08-28 17:45:38 -0300

    correcao interface

--- src/main/java/src/Interface/Main.java
+++ src/main/java/src/Interface/Main.java
@@ -32,6 +32,7 @@ public class Main extends PApplet {
     PImage rl;
     PImage rr;
     PImage logo;
+    PImage logoError;
     String debuga = "x: " + mouseX + " y: " + mouseY;
     int[] backgroundColor = {0,0,0};
     int[] textColor = {255,255,255};
@@ -97,6 +98,7 @@ public class Main extends PApplet {
         imgCentral = defaultImage = loadImage("src/main/java/src/Interface/resources/default.png");
         fonte = createFont("src/main/java/src/Interface/resources/fonteUltrakill.ttf", 16);
         logo = loadImage("src/main/java/src/Interface/resources/logo.jpg");
+        logoError = loadImage("src/main/java/src/Interface/resources/logoError.jpg");
     }
 
     @Override
@@ -107,37 +109,48 @@ public class Main extends PApplet {
             if(!unecessaryLoading.check1 && unecessaryLoading.conectado)unecessaryLoading.printaPontosCPU();
             if(unecessaryLoading.check1)unecessaryLoading.printaPontosGPU();
         }else {
-            updateDados();
+            if(Var.clienteConectado){
+                updateDados();
 
-            background(0, 0, 0);
-            background(0, 0, 0);
-            image(imgCentral, (((largura - 240) / 2) - (larguraImagem / 2)), ((altura / 2) - (alturaImagem / 2)));
-            display();
-            if (!mouseOver()) gerenciadorImagens(indexCaixa);
-            fill(corSist[0], corSist[1], corSist[2]);
-            textSize(50);
-            text(sistemaOn, (largura / 2) - 400, 80);
-            fill(0, 0, 0);
-            stroke(255);
-            rect(960 - 240, 0, largura, altura);
-            rect(960 - 240, 40, largura, altura);
-            textSize(20);
-            fill(255);
-            text("Dados adicionais", 965 - 240, 35);
-            textSize(16);
-            text("altura media frente: " + alturaFMeio, 965 - 240, 65);
-            text("altura media atras: " + alturaRMeio, 965 - 240, 95);
-            text("Acelerometro: " + acelerometro, 965 - 240, 120);
-            text("sensor distancia fr: " + sensorDistFr + "mm", 965 - 240, 145);
-            text("sensor distancia rr: " + sensorDistRR + "mm", 965 - 240, 170);
-            text("sensor distancia fl: " + sensorDistFL + "mm", 965 - 240, 195);
-            text("sensor distancia rl: " + sensorDistRL + "mm", 965 - 240, 220);
-            fill(colorEnviar);
-            rect(largura - 200, altura - 80, 160, 40 );
-            fill(255- colorEnviar);
-            text("Enviar", largura - 150, altura - 55);
-            colorEnviar = 0;
-            image(logo, 10, altura-100);
+                background(0, 0, 0);
+                background(0, 0, 0);
+                image(imgCentral, (((largura - 240) / 2) - (larguraImagem / 2)), ((altura / 2) - (alturaImagem / 2)));
+                display();
+                if (!mouseOver()) gerenciadorImagens(indexCaixa);
+                fill(corSist[0], corSist[1], corSist[2]);
+                textSize(50);
+                text(sistemaOn, (largura / 2) - 400, 80);
+                fill(0, 0, 0);
+                stroke(255);
+                rect(960 - 240, 0, largura, altura);
+                rect(960 - 240, 40, largura, altura);
+                textSize(20);
+                fill(255);
+                text("Dados adicionais", 965 - 240, 35);
+                textSize(16);
+                text("altura media frente: " + alturaFMeio, 965 - 240, 65);
+                text("altura media atras: " + alturaRMeio, 965 - 240, 95);
+                text("Acelerometro: " + acelerometro, 965 - 240, 120);
+                text("sensor distancia fr: " + sensorDistFr + "mm", 965 - 240, 145);
+                text("sensor distancia rr: " + sensorDistRR + "mm", 965 - 240, 170);
+                text("sensor distancia fl: " + sensorDistFL + "mm", 965 - 240, 195);
+                text("sensor distancia rl: " + sensorDistRL + "mm", 965 - 240, 220);
+                fill(colorEnviar);
+                rect(largura - 200, altura - 80, 160, 40 );
+                fill(255- colorEnviar);
+                text("Enviar", largura - 150, altura - 55);
+                colorEnviar = 0;
+                image(logo, 10, altura-100);
+            }else{
+                unecessaryLoading.disconnect(logoError);
+                delay(2000);
+                unecessaryLoading.concluido = false;
+                unecessaryLoading.check1 = false;
+                unecessaryLoading.check2 = false;
+                unecessaryLoading.cont = 0;
+                unecessaryLoading.contPontos1 = 0;
+                unecessaryLoading.contPontos2 = 0;
+            }
 
         }
 
